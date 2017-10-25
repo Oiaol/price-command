7b9c09884b98e3d5c0ea8f6e52826991ebcc870e
Oiaol <33039757+Oiaol@users.noreply.github.com>
2017-10-25 23:59:38 +0100

    Update price.js

--- price.js
+++ price.js
@@ -1,4 +1,4 @@
-
+// require all exchange files to call later on with Promise.all
 const coins = {
 	bitfinex: require("./exchanges/bitfinex.js"),
 	yobit: require("./exchanges/yobit.js"),
@@ -12,16 +12,18 @@ const coins = {
 	cryptopia: require("./exchanges/cryptopia.js")
 };
 
-
+// price async function taking a param (coin) which is the ticker 
 const price = async (coin) => {
-	const testHyphen = new RegExp("\\w{1,}[\\-]\\b", "g");
+	const testHyphen = new RegExp("\\w{1,}[\\-]\\b", "g"); // test if it includes a "-"
 	let tickerVol = coin.split("-")[1] || false;
-	const onExchanges = await Promise.all(Object.values(coins).map(fn => fn(coin)));
-	if(onExchanges.some(value => value)) {
+	const onExchanges = await Promise.all(Object.values(coins).map(fn => fn(coin))); 
+	/* check if its on any exchange by awaiting all the exchanges defined in a object as coins
+	and using map to call each function in the object coins with the parameter given which is coin. */
+	if(onExchanges.some(value => value)) { // if onExchanges contains any information then continue
 		if(testHyphen.test(coin)) tickerVol = coin.split("-")[1];
 		else if(coin.startsWith("$")) tickerVol = "USD";
 		else tickerVol = "BTC";
-		const volumeExchanges = {
+		const volumeExchanges = { // storing exchange ticker volume if its returned else its false.
 				bitfinex: onExchanges[Object.keys(coins).indexOf("bitfinex")][0] || false,
 				yobit: onExchanges[Object.keys(coins).indexOf("yobit")][0] || false,
 				binance: onExchanges[Object.keys(coins).indexOf("binance")][0] || false,
@@ -34,18 +36,10 @@ const price = async (coin) => {
 				cryptopia: onExchanges[Object.keys(coins).indexOf("cryptopia")][0] || false
 			},
 			max = Object.keys(volumeExchanges).reduce((a, b) => volumeExchanges[a] > volumeExchanges[b] ? a : b),
-			tickerExchanges = {
-				bitfinex: onExchanges[Object.keys(coins).indexOf("bitfinex")],
-				yobit: onExchanges[Object.keys(coins).indexOf("yobit")],
-				binance: onExchanges[Object.keys(coins).indexOf("binance")],
-				kraken: onExchanges[Object.keys(coins).indexOf("kraken")],
-				bitstamp: onExchanges[Object.keys(coins).indexOf("bitstamp")],
-				liqui: onExchanges[Object.keys(coins).indexOf("liqui")],
-				hitbtc: onExchanges[Object.keys(coins).indexOf("hitbtc")],
-				bittrex: onExchanges[Object.keys(coins).indexOf("bittrex")],
-				polo: onExchanges[Object.keys(coins).indexOf("poloniex")],
-				cryptopia: onExchanges[Object.keys(coins).indexOf("cryptopia")]
-			};
-		console.log(`**Ticker:** ${coin}\n**Volume in ${tickerVol}:** ${parseFloat(volumeExchanges[max]).toLocaleString()}\n**Last Price:** ${tickerExchanges[max][1]}\n**Ask Price:** ${tickerExchanges[max][2]}\n**Bid Price:** ${tickerExchanges[max][3]}\n**High Price:** ${tickerExchanges[max][4] || false}\n**Low Price:** ${tickerExchanges[max][5] || false}`);
+		      /* max = caclulate the highest volume in given object which returns the key which can be used later on 
+		      in the code to extract futher information about the coin in the array. */
+		      //now we can max to get more information about the coin including last price ask price bid price etc.
+		      tickerExchange = onExchange[Object.keys(coins).indexOf(max)] // this now includes the information from the highest volume exchange of the ticker given.
+		console.log(`**Ticker:** ${coin}\n**Volume in ${tickerVol}:** ${parseFloat(volumeExchanges[max]).toLocaleString()}\n**Last Price:** ${tickerExchange[1]}\n**Ask Price:** ${tickerExchange[2]}\n**Bid Price:** ${tickerExchange[3]}\n**High Price:** ${tickerExchange[4] || false}\n**Low Price:** ${tickerExchange[5] || false}`);
 	}
 };
